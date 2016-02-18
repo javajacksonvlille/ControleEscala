05db29a9ec2ede9187be324196690ff4f78d7b63
thiag <plxprado@gmail.com>
2016-02-18 00:25:35 -0200

    first commit

---
+++ src/test/java/com/controleescala/domain/DomainTest.java
@@ -0,0 +1,22 @@
+package com.controleescala.domain;
+
+//@ContextConfiguration(classes = {PersistenceContext.class})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class DomainTest {
+
+//    @Autowired DistanciaRepository distanciaRepository;
+//    @Autowired PontoRepository pontoRepository;  
+//    @Autowired Session session;
+
+//    @Test
+    public void salvarPonto() {
+//        Ponto testPontoA = new Ponto("TEST_A");
+//        pontoRepository.save(testPontoA);
+//        Ponto fromBDTesteA = pontoRepository.findByNome("TEST_A");
+//        assertEquals(testPontoA.getNome(), fromBDTesteA.getNome());
+    }
+    
+   
+    
+}
