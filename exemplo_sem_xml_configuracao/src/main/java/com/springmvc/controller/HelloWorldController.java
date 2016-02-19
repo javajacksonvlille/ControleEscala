9094b072f6970d31fa56c2828f05f3851a6a2bd8
rogeriovolsi <rogeriovolsi@192.168.1.32>
2016-02-19 21:55:52 -0200

    Exemplo de configuracao sem XML, apenas com annotation.

---
+++ exemplo_sem_xml_configuracao/src/main/java/com/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+	
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+		model.addAttribute("result1", "teste");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+        model.addAttribute("result1", "teste2");
+        return "welcome";
+    }
+    
+
+}
